8431e7337a89c5ac059ca35c52d66a294c0e3cc1
Jixlen999 <viktoriyaivanyutenko@yandex.ru>
2024-11-17 00:09:54 +0300

    feat: install playwright, write test, setup workflow

---
+++ playwright/example.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("has button", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByRole("button", { title: "get note" })).toBeVisible();
+});

5191b4f8d03ef1b2f6d4c22030bdb35d0f1cbc63
Jixlen999 <viktoriyaivanyutenko@yandex.ru>
2024-12-19 15:05:27 +0300

    feat: update test

--- playwright/example.spec.js
+++ playwright/example.spec.js
@@ -3,7 +3,12 @@ import { test, expect } from "@playwright/test";
 test("has button", async ({ page }) => {
   await page.goto("/");
 
-  await expect(page.getByRole("button", { title: "get note" })).toBeVisible();
+  const button = await page.getByRole("button", { title: "get note" });
 
-  await expect(page).toHaveScreenshot("button.png");
+  await expect(button).toBeVisible();
+
+  await button.click();
+
+  await expect(page.getByText("First Note")).toBeVisible();
+  await expect(page.getByText("Second Note")).toBeVisible();
 });
